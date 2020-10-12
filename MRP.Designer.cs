7b433592bcd61da4531e2bb9754ed9108d2f63bb
AceHFIT <69657548+AceHFIT@users.noreply.github.com>
2020-10-12 10:18:08 -0500

    Add files via upload

---
+++ MRP.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MarsRoverPhonos
+{
+    partial class MRP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btClose = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btGetPhotos = new System.Windows.Forms.Button();
+            this.imgLstNasa = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(674, 415);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 0;
+            this.btClose.Text = "&Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(301, 12);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 1;
+            this.dtpDate.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
+            // 
+            // btGetPhotos
+            // 
+            this.btGetPhotos.Enabled = false;
+            this.btGetPhotos.Location = new System.Drawing.Point(301, 415);
+            this.btGetPhotos.Name = "btGetPhotos";
+            this.btGetPhotos.Size = new System.Drawing.Size(200, 23);
+            this.btGetPhotos.TabIndex = 2;
+            this.btGetPhotos.Text = "&Retriev Photos";
+            this.btGetPhotos.UseVisualStyleBackColor = true;
+            this.btGetPhotos.Click += new System.EventHandler(this.btGetPhotos_Click);
+            // 
+            // imgLstNasa
+            // 
+            this.imgLstNasa.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imgLstNasa.ImageSize = new System.Drawing.Size(16, 16);
+            this.imgLstNasa.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // MRP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btGetPhotos);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.btClose);
+            this.Name = "MRP";
+            this.Text = "Mars Rover Photos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btGetPhotos;
+        private System.Windows.Forms.ImageList imgLstNasa;
+    }
+}
+
---
+++ MRP.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace MarsRoverPhonos
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Camera
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int rover_id { get; set; }
+        public string full_name { get; set; }
+    }
+
+    public class Rover
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string landing_date { get; set; }
+        public string launch_date { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Photo
+    {
+        public int id { get; set; }
+        public int sol { get; set; }
+        public Camera camera { get; set; }
+        public string img_src { get; set; }
+        public string earth_date { get; set; }
+        public Rover rover { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Photo> photos { get; set; }
+    }
+
+
+
+    public partial class MRP : Form
+    {
+        string jsonData;
+        string theDate;
+        public MRP()
+        {
+            InitializeComponent();
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void GetPhotos()
+        {
+            string sUrl;
+            sUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + theDate + "&api_key=DEMO_KEY";
+            try
+            {
+                System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(sUrl);
+                using (System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse())
+                {
+
+                    using (System.IO.Stream stream = response.GetResponseStream())
+                    using (System.IO.StreamReader reader = new System.IO.StreamReader(stream))
+                    {
+                        jsonData = reader.ReadToEnd();
+                    }
+
+                    var parent = JObject.Parse(jsonData);
+                    var theParent = JsonConvert.DeserializeObject<Root>(jsonData);
+
+                    string url = theParent.photos[0].img_src;
+                    WebClient webClient = new WebClient();
+                    string fileName = Path.GetFileName(url);
+
+                    if (Directory.Exists(Directory.GetCurrentDirectory() + "\\images"))
+                        Directory.Delete(Directory.GetCurrentDirectory() + "\\images");
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + "\\images");
+                    webClient.DownloadFile(url, Directory.GetCurrentDirectory() + "\\images\\" + fileName);
+                    System.Drawing.Image myImage = Image.FromFile(Directory.GetCurrentDirectory()  + @"\\images\\" + fileName);
+                    imgLstNasa.Images.Add(myImage);
+                    this.BackgroundImage = myImage;
+                    
+
+                }
+            }
+            catch(HttpRequestException e)
+            {
+
+            }
+        }
+
+        public System.Drawing.Image DownloadImageFromUrl(string imageUrl)
+        {
+            System.Drawing.Image image = null;
+
+            try
+            {
+                System.Net.HttpWebRequest webRequest = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                System.Net.WebResponse webResponse = webRequest.GetResponse();
+
+                System.IO.Stream stream = webResponse.GetResponseStream();
+
+                image = System.Drawing.Image.FromStream(stream);
+
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return image;
+        }
+        private void btGetPhotos_Click(object sender, EventArgs e)
+        {
+            GetPhotos();
+        }
+
+        private void dtpDate_ValueChanged(object sender, EventArgs e)
+        {
+            dtpDate.Format = DateTimePickerFormat.Custom;
+            dtpDate.CustomFormat = "yyyy-dd-MM";
+            theDate = dtpDate.Text;
+            btGetPhotos.Enabled = true;
+        }
+    }
+}
